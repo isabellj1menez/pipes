3bc969fb1e168629c75e6069e89f1f5ec010a749
Alejandra <isabell_alejandra@hotmail.com>
2019-11-08 16:27:11 -0600

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public Nombre: string = "Isabel Alejandra Perez Jimenez";
+  public Nombre: string = "Isabel Jimenez";
   public arreglo: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   public pi : number = Math.PI;
   public porcentaje: number = 0.245;
   public salario = 1234.5;
   public fecha = new Date();
   public persona={
-    nombre:'IA',
-    edad:25,
+    nombre:'IJ',
+    edad:56,
     direccion:{
-      calle:'Cerro de los gallos',
-      casa:'213'
+      calle:'mi callle',
+      casa:'457'
     }
   }
-  public nombre2:string='IsAbEl AlEjAnDra PeRez JiMenEz'
+  public nombre2:string='IsAbEl JiMenEz'
 }
