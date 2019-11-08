7e58070a4a6e073d99609e4507961c0a6b9d7808
Alejandra <isabell_alejandra@hotmail.com>
2019-11-08 16:20:18 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  public Nombre: string = "Isabel Alejandra Perez Jimenez";
+  public arreglo: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public pi : number = Math.PI;
+  public porcentaje: number = 0.245;
+  public salario = 1234.5;
+  public fecha = new Date();
+  public persona={
+    nombre:'IA',
+    edad:25,
+    direccion:{
+      calle:'Cerro de los gallos',
+      casa:'213'
+    }
+  }
+  public nombre2:string='IsAbEl AlEjAnDra PeRez JiMenEz'
 }
